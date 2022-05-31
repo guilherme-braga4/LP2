e4a61ad5aff2b27a4b11fd93e405b5585448127a
guilherme-braga4 <90586912+guilherme-braga4@users.noreply.github.com>
2022-05-31 10:21:40 -0300

    Prova C# - LP2 - Guilherme Braga e Caroline Braga

---
+++ Prova/PNotas/PNotas/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace PNotas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.lstNotas = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.btnVerificar.Location = new System.Drawing.Point(192, 242);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(147, 58);
+            this.btnVerificar.TabIndex = 0;
+            this.btnVerificar.Text = "Verificar";
+            this.btnVerificar.UseVisualStyleBackColor = true;
+            this.btnVerificar.Click += new System.EventHandler(this.btnVerificar_Click);
+            // 
+            // lstNotas
+            // 
+            this.lstNotas.FormattingEnabled = true;
+            this.lstNotas.Location = new System.Drawing.Point(485, 12);
+            this.lstNotas.Name = "lstNotas";
+            this.lstNotas.Size = new System.Drawing.Size(324, 654);
+            this.lstNotas.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1025, 698);
+            this.Controls.Add(this.lstNotas);
+            this.Controls.Add(this.btnVerificar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnVerificar;
+        private System.Windows.Forms.ListBox lstNotas;
+    }
+}
+
---
+++ Prova/PNotas/PNotas/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+
+namespace PNotas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVerificar_Click(object sender, EventArgs e)
+        {
+            //declarando a variável tipo matriz que vai ter x e y ou seja, 2 dimensões
+            char[,] matrizNotas = new char[8, 10];
+            string auxiliar;
+            char resposta;
+            int numeroQuestao = 0;
+            char[] gabarito = new char[10]{'A','B','C','D','A','C','D','A','B','C'};
+
+       
+            for (var i = 0; i < 8; i++)
+            {
+                numeroQuestao = 0;
+
+                for (var j = 0; j < 10; j++)
+                {
+                    auxiliar = Interaction.InputBox("Digite a resposta da questão: " + (j + 1), "Entrada das respostas");
+                    
+                    if (char.TryParse(auxiliar, out matrizNotas[i, j]))
+                    {
+                        if ((matrizNotas[i, j] != 'A') && (matrizNotas[i, j] != 'B') && (matrizNotas[i, j] != 'C') && (matrizNotas[i, j] != 'D') && (matrizNotas[i, j] != 'E'))
+                        {
+                            MessageBox.Show("Resposta inválida");
+
+                            j--;
+                        }
+
+                        else
+                        {
+                            resposta = matrizNotas[i, j]; 
+
+                            numeroQuestao++; 
+
+                            if (gabarito[j] == resposta)
+                            {
+                                lstNotas.Items.Add("O Aluno: " + (i + 1) + " acertou a questão: " + (numeroQuestao) + " era " + gabarito[j] + " escolheu: " + resposta);
+                            }
+                            else lstNotas.Items.Add("O Aluno: " + (i + 1) + " errou a questão: " + (numeroQuestao) + " era " + gabarito[j] + " escolheu: " + resposta);                     
+
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Nota inválida");
+                        j--;
+                    }
+                }
+            }
+        }
+    }
+}
